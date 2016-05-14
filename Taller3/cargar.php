355997b7b63d93a1e5101126d9f1d9749741dc76
JESUS JIMENEZ <donchubi94@gmail.com>
2016-05-14 18:01:26 -0430

    Procesamiento de articulos, calculo de totales, elaboracion y retorno de factura, modificaciones en la validacion, falta probar los php

--- Taller3/cargar.php
+++ Taller3/cargar.php
@@ -3,8 +3,67 @@
    if($con->connect_error)
       die('error conectando a bd: '.$con->connect_error);
 
-   $art = $con->query('select codigo, nombre, precio from articulos');
+   $art = $con->query('select * from articulos where codigo = '.$_GET['codigo']);
    $con->close();
 
-   // modificar para que gaha la consulta referente a la consulta del usuario
+   if ($q=$art->fetch_assoc())
+   {
+      $json_file = file_get_contents('factura.json');
+      if ($json_file !== false) {
+         $factura = json_decode($json_file, true);
+      } else {
+         $factura = array();
+      }
+      $codigo=$q['codigo'];
+      $cantidad=$_GET['amount'];
+      if ($factura != NULL && array_key_exists($codigo, $factura))
+      {
+         $factura[$codigo]['cantidad']+=$cantidad;
+         $factura[$codigo]['preciototal']=$factura[$codigo]['precio']*$factura[$codigo]['cantidad'];
+      } else
+      {
+         $nombre=$q['nombre'];
+         $precio=$q['precio'];
+         $factura[$codigo]=array();
+         $factura[$codigo]['nombre']=$nombre;
+         $factura[$codigo]['precio']=$precio;
+         $factura[$codigo]['cantidad']=$cantidad;
+         $factura[$codigo]['preciototal']=$precio*$cantidad;
+      }
+
+      file_put_contents('factura.json', json_encode($factura));
+
+      echo '<table>';
+      echo '<tr>';
+      echo '<th>Código</th>';
+      echo '<th>Cantidad</th>';
+      echo '<th>Descripción</th>';
+      echo '<th>Precio Unitario</th>';
+      echo '<th>Precio Total</th>';
+      echo '</tr>';
+      $subtotal=0;
+      foreach($factura as $codigo => $articulo)
+      {
+         echo '<tr>';
+         echo '<td>'.$codigo.'</td>';
+         echo '<td>'.$articulo['cantidad'].'</td>';
+         echo '<td>'.$articulo['nombre'].'</td>';
+         echo '<td>'.$articulo['precio'].'</td>';
+         echo '<td>'.$articulo['preciototal'].'</td>';
+         $subtotal+=$articulo['preciototal'];
+         echo '</tr>';
+      }
+      echo '<tr>';
+      echo '<th>Subtotal</th>';
+      echo '<th>IVA</th>';
+      echo '<th>Total</th>';
+      echo '</tr>';
+      echo '<tr>';
+      echo '<td>'.$subtotal.'</td>';
+      $iva=$subtotal*0.12;
+      echo '<td>'.$iva.'</td>';
+      echo '<td>'.$subtotal+$iva.'</td>';
+      echo '</tr>';
+      echo '</table>';
+   }
  ?>
--- Taller3/taller3.js
+++ Taller3/taller3.js
@@ -9,10 +9,17 @@ function enviarDatos()
       {
          if (xhttp.readyState == 4 && xhttp.status == 200)
          {
-            document.getElementById("facturacion").innerHTML = xhttp.responseText;
+            var responseDoc = xhttp.responseText;
+            if (responseDoc !== '')
+            {
+               document.getElementById("facturacion").innerHTML = responseDoc;
+            } else
+            {
+               codigoInvalido();
+            }
          }
       };
-      xhttp.open("GET", "cargar.php", true);
+      xhttp.open("GET", "cargar.php?code="+input[0].value+"&amount="+input[1].value, true);
       xhttp.send();
    }
 }
@@ -22,17 +29,24 @@ function validarEntrada(input)
    var result=true;
    var span = limpiarSpans();
 
-   if(input[0].value === '')
+   if(input[0].value === '' || input[0].value <= 0)
    {
       span[0].style = 'color:red';
-      span[0].innerHTML = ' <= Debes llenar el campo con numeros!!';
+      span[0].innerHTML = ' <= Debes llenar el campo con numeros positivos!!';
       result = false;
-   }
-   if(input[1].value === '')
+   } else
    {
-      span[1].style = 'color:red';
-      span[1].innerHTML = ' <= '+(result?'Debes llenar el campo con numeros!!':'Este tambien!!');
-      result = false;
+      if(input[1].value === '' || input[1].value <= 0)
+      {
+         input[1].value=1;
+      } else
+      {
+         if(input[1].value > 9)
+         {
+            span[1].style = 'color:green';
+            span[1].innerHTML = ' Vaya! Cuántos articulos!';
+         }
+      }
    }
    return result;
 }
@@ -43,4 +57,12 @@ function limpiarSpans()
       spans[0].innerHTML='';
       spans[1].innerHTML='';
       return spans;
+}
+
+function codigoInvalido()
+{
+   var span = limpiarSpans();
+
+   span[0].style = 'color:red';
+   span[0].innerHTML = ' <= Código inválido - pulsa ayuda para ver los productos y sus códigos';
 }

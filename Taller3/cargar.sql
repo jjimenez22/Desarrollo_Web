a0a462ec154930f07099c488e47ef76519fdbe5a
jjimenez22 <jgjimenezb94@gmail.com>
2016-05-14 22:18:30 +0000

    corregido error en sql, separada creacion de carga de datos, error en fetch assoc

---
+++ Taller3/cargar.sql
@@ -0,0 +1,13 @@
+ \r articulos
+
+INSERT INTO articulos (nombre, precio) VALUES
+('Morral', 2500),
+('Cartucheras', 500),
+('Libreta Materias', 900),
+('Oso de peluche', 1800),
+('Zarcillos', 750),
+('Pulseras tejidas', 1650),
+('Zapatos deportivos', 8000),
+('Vestido corto', 4500),
+('Vestido largo noche', 7000),
+('Gorra', 400);
--- Taller3/crear.sql
+++ Taller3/crear.sql
@@ -7,15 +7,3 @@ create database articulos;
          precio float not null,
          primary key (codigo)
       );
-
-INSERT INTO articulos (nombre, precio) VALUES
-('Morral', 2500, 20),
-('Cartucheras', 500, 15),
-('Libreta Materias' 8, 900, 30),
-('Oso de peluche', 1800, 10),
-('Zarcillos', 750, 3),
-('Pulseras tejidas', 1650, 10),
-('Zapatos deportivos', 8000, 20),
-('Vestido corto', 4500, 10),
-('Vestido largo noche', 7000, 2),
-('Gorra', 400, 25);
